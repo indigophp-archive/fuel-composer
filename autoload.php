17ea6f69f2909d1d3f90870eedb90f0099a1e7d1
Sági-Kazár Márk <mark.sagikazar@gmail.com>
2014-06-26 23:16:36 +0200

    Minor changes

--- autoload.php
+++ autoload.php
@@ -1,5 +1,22 @@
 <?php
 
+/*
+ * This file is part of the Fuel Composer package.
+ *
+ * (c) Indigo Development Team
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * Composer CustomLoader class
+ *
+ * Makes it possible to autoload a class and call a static function as well
+ *
+ * @link https://github.com/composer/composer/issues/1493#issuecomment-12492276
+ * @author Márk Sági-Kazár <mark.sagikazar@gmail.com>
+ */
 class CustomLoader
 {
     private $loader;
